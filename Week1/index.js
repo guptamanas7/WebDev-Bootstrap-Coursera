63065fafc1e95f458747df13540ab17ed51700a3
MANAS GUPTA <g7manas@gmail.com>
2022-12-25 12:16:23 +0530

    Second commit

---
+++ Week1/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (request, response) =>
+  response.sendFile(index.html));
+app.post("/", function (req, res) {
+  var n1 = Number(req.body.n1);
+  var n2 = Number(req.body.n2);
+  var result = n1 + n2;
+  res.send("The Result of your calculation is: " + result);
+});
+
+app.listen(8000, function () {
+  console.log("Server is running on port 8000");
+});
